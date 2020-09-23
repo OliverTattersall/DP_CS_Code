862842b7c4fb4e21980fa722acfe5763af643988
oliver tattersall <oliver.tattersall@GCCYJQ0VJ1WK.local>
2020-09-23 10:55:35 -0400

    findPrimes function

--- Tools/toolsOliverTattersall.py
+++ Tools/toolsOliverTattersall.py
@@ -307,3 +307,39 @@ def writeListToFile(lst, str):
         myfile.write(lst[i])
 
 # writeListToFile(["hello", "mr", 'johnson'], "test")
+
+
+'''
+Function takes integer n and finds all primes between 0 and n inclusive
+
+Parameters: Integer n
+
+Conditions: n>2
+'''
+
+
+def findPrimes(n):
+    
+    primes=[] #list where all primes will go into
+    prime=[True for i in range(n+1)] #list intiated with n+1 True values
+    p=2 # initial number to search through as all multiples of 0 and 1 are 0 and 1 and will never increase
+
+    while p*p<=n: #loops from 2 to √n as the highest possible factor that would not be 
+                  #eliminated would be √n. Ex. 36, any factors higher than 6 would have been eliminated already: 18 by 2, 12 by 3
+        if prime[p]==True: #tests if the current p value is a prime so it can eliminate all multiples
+            
+            for i in range(p*p, n+1, p): # loops from 
+                prime[i]=False
+
+        p+=1 # increments p to test the next integer
+
+
+    for i in range(2, n+1, 1): # loops from 2 to n
+        if prime[i]==True: # tests to see if that number is prime
+            primes.append(i) # if prime appends it to the list primes
+
+    return primes #returns list of primes between 2 and n inclusive
+
+# testcase
+# print(findPrimes(13))
+

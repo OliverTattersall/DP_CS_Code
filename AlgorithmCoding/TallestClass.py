79df7c3581bf1daa3132ae23e678abe163bc7670
oliver tattersall <oliver.tattersall@GCCYJQ0VJ1WK.local>
2021-01-25 13:58:51 -0500

    update

--- AlgorithmCoding/TallestClass.py
+++ AlgorithmCoding/TallestClass.py
@@ -6,22 +6,33 @@
 #     people.append(input().split())
 
 myfile=open("data.txt", "r")
-item=myfile.read().split("\n")
+
+item=myfile.read()
+# print(item)
+item=item.split("\n")
+# print(item)
+myfile.close()
+
 N=int(item[0])
 people=[]
 
 for i in range(1, N+1):
+
     people.append(item[i].split())
 
+# for r in range(0, len(people), 1):
+#     print(people[r])
+
 cons={"m":1, "dm":0.1, "cm":0.01, "mm":0.001}
-myfile.close()
 
-
+# converting everything to meters 
 for i in range(N):
     people[i][1]=float(people[i][1])*cons[people[i][2]]
 
 people.sort(key = lambda x:x[1])
-# print(people)
+
+
+# people = sorted(people)
 
 for i in range(N-1, N-6, -1):
     print(people[i][0])
